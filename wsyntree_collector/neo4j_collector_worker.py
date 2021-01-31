7c95ca3405bf693391a35fa5eccb6edca694f06a
Ben Klein <robobenklein@gmail.com>
2021-01-31 13:33:07 -0500

    handle DatabaseError on text insert

--- wsyntree_collector/neo4j_collector_worker.py
+++ wsyntree_collector/neo4j_collector_worker.py
@@ -1,11 +1,14 @@
 
 from pathlib import Path
 
+import neo4j
+
 from wsyntree import log
 from wsyntree.tree_models import (
     SCM_Host, WSTRepository, File, WSTNode, WSTText
 )
 from wsyntree.wrap_tree_sitter import get_TSABL_for_file
+
 
 def _process_file(path: Path, tree_repo: WSTRepository):
     file = File(
@@ -31,7 +34,6 @@ def _process_file(path: Path, tree_repo: WSTRepository):
     cursor = tree.walk()
     # iteration loop
     cur_tree_parent = None
-    # prev_tree_node = None
     while cursor.node is not None:
         cur_node = cursor.node
         nn = WSTNode(
@@ -45,20 +47,23 @@ def _process_file(path: Path, tree_repo: WSTRepository):
         nn.file.connect(file)
         if cur_tree_parent:
             nn.parent.connect(cur_tree_parent)
-        # TODO text storage
+        # text storage
         try:
             text = WSTText.get_or_create({
                 'text': cur_node.text.tobytes().decode()
             })[0]
             nn.text.connect(text)
+        except neo4j.exceptions.DatabaseError as e:
+            # Neo4j imposes an internal hard limit of 4039 bytes to string properties
+            # https://neo4j.com/docs/operations-manual/current/performance/index-configuration/#index-configuration-btree-limitations-key-sizes
+            log.debug(f"{file}:{nn} text content not stored")
+            file.error = "WST_TEXT_STORE_FAILED"
+            file.save()
         except UnicodeDecodeError as e:
             log.warn(f"{file}:{nn} failed to decode content")
             file.error = "UNICODE_DECODE_ERROR"
             file.save()
             return
-        # if cur_tree_parent is not None:
-        #     cur_tree_parent.children.append(nn)
-        #     cur_tree_parent.save()
 
         # now determine where to move to next:
         next_child = cursor.goto_first_child()
