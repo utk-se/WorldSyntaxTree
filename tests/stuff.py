75fe35847991d2b50d6f10ea240d23efa1a3f2df
Ben Klein <robobenklein@gmail.com>
2021-02-24 17:16:54 -0500

    test.py: print all

--- tests/stuff.py
+++ tests/stuff.py
@@ -1,5 +1,13 @@
 
+import antigravity
+from space import ship
+from earth.humans import code
+
 def hello(name):
     print("hi" + name)
+
+for x in y:
+    for z in x:
+        print(z)
 
 hello("ben")
