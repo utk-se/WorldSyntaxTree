d60060a19ceef411c1e341aff2c3ab604599c0c9
Ben Klein <bklein3@vols.utk.edu>
2023-04-10 01:22:22 -0400

    iterating blobs with filename filter

--- wsyntree_collector/commands/woc/nhv1_blobfuncs_v0.py
+++ wsyntree_collector/commands/woc/nhv1_blobfuncs_v0.py
@@ -5,14 +5,18 @@ This "command" is only to be run as a job on the DA cluster, not used as a subco
 import argparse
 import subprocess
 from typing import List, Optional
-from pathlib import Path
+from pathlib import Path, PurePath, PurePosixPath
+from collections import Counter, namedtuple
 
 import networkx as nx
 from networkx.readwrite import json_graph
 import orjson
+import pebble
 
 from wsyntree import log
-from wsyntree.wrap_tree_sitter import TreeSitterAutoBuiltLanguage, TreeSitterCursorIterator
+from wsyntree.wrap_tree_sitter import (
+    TreeSitterAutoBuiltLanguage, TreeSitterCursorIterator, get_TSABL_for_file,
+)
 from wsyntree.hashtypes import WSTNodeHashV1
 
 from wsyntree_collector.file.parse_file_treesitter import build_networkx_graph
@@ -21,23 +25,69 @@ from wsyntree_collector.wociterators import all_blobs as all_blobs_iterator
 #import oscar
 
 
+getValuesScript = Path("~/lookup/getValues").expanduser()
+assert getValuesScript.exists(), f"Expected getValues at {getValuesScript}"
 output_root_dir = Path("/da7_data/WorldSyntaxTree/nhv1/blobfuncs_v0")
 assert output_root_dir.is_dir(), f"Output directory does not exist."
 
 
-def get_filenames_for_blob(b: str) -> List[Path]:
-    """Inefficient version: calls lookup b2f because I am lazy to rewrite it in python"""
-    c = subprocess.run(
-        ["bash", getValuesScript, "b2f"],
-        stdin=f"{b}\n",
-        text=True,
-        capture_output=True,
-    )
+#def get_filenames_for_blob(b: str) -> List[PurePosixPath]:
+#    """Inefficient version: calls lookup b2f because I am lazy to rewrite it in python"""
+#    c = subprocess.run(
+#        ["bash", getValuesScript, "b2f"],
+#        input=f"{b}\n",
+#        text=True,
+#        capture_output=True,
+#    )
+#    if c.returncode != 0:
+#        log.trace(log.warn, c.stdout)
+#        log.trace(log.warn, c.stderr)
+#    c.check_returncode()
+#    lines = c.stdout.strip().split("\n")
+#    if len(lines) != 1:
+#        log.warn(f"getValues gave {len(lines)} lines for {b}")
+#    filenames = []
+#    for line in lines:
+#        for fname in line.split(";")[1:]:
+#            filenames.append(PurePosixPath(fname))
+#    return filenames
 
+def wst_supports_fnames(fnames):
+    filenames = fnames
+    if not filenames:
+        return False
+    exts = Counter([x.suffix for x in filenames if x.suffix])
+    primary_ext = exts.most_common(1)
+    if not primary_ext:
+        return False
+    primary_ext = primary_ext[0][0]
+    if get_TSABL_for_file(str(primary_ext)) is not None:
+        return True
+    log.debug(f"WST {primary_ext} not supported: {filenames[0]}")
+    return False
+
+def run_blob(blob_pair):
+    """"""
+    blob, content, filenames = blob_pair
+    if not filenames:
+        return (None, "NO_FILENAMES")
+    exts = Counter([x.suffix for x in filenames if x.suffix])
+    primary_ext = exts.most_common(1)
+    if not primary_ext:
+        return (None, "NO_FILENAME_SUFFIX")
+    primary_ext = primary_ext[0][0]
+    tsabl = get_TSABL_for_file(str(primary_ext)) # pebble synchronized cache
+    log.debug(f"Start parsing {blob} with {tsabl}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("WST Collector NHV1 BlobFuncs v0")
+    parser.add_argument("-w", "--workers", type=int, default=4)
+    parser.add_argument("-v", "--verbose", action="store_true")
+    args = parser.parse_args()
+    if args.verbose:
+        log.setLevel(log.DEBUG)
 
-    args = parser.parser_args()
+    blobs = all_blobs_iterator(filefilter=lambda f: wst_supports_fnames(f))
 
-    print(get_filenames_for_blob(0194e44034e001afd2bdb3b54e6c11a288e25806))
+    with pebble.ThreadPool(max_workers=args.workers) as pool:
+        pool.map(run_blob, blobs)
--- wsyntree_collector/wociterators.py
+++ wsyntree_collector/wociterators.py
@@ -1,6 +1,8 @@
 import sys
 import os
-from pathlib import Path
+import gzip
+from pathlib import Path, PurePosixPath
+from collections import namedtuple
 
 import oscar
 
@@ -9,25 +11,36 @@ all_blob_sections = range(0, blob_sections)
 
 blob_fbase = "/da5_data/All.blobs/blob_{section}.{ext}"
 
-def iter_blobs(section):
-    p_idx = Path(blob_fbase.format(section=section, ext="idx"))
+blobresult = namedtuple("BlobResult", ["hash", "content", "filenames"])
+
+def iter_blobs(section, blobfilter=lambda b: True, filefilter=lambda fnames: True):
+    """
+    blobfilter(str) -> bool
+    filefilter(List[PurePosixPath]) -> bool
+
+    all provided filters must pass
+    """
+    p_idx = Path(blob_fbase.format(section=section, ext="idxf"))
     p_bin = Path(blob_fbase.format(section=section, ext="bin"))
 
-    with p_idx.open("rt") as idx_f, p_bin.open("rb") as bin_f:
+    with gzip.open(p_idx, "rt") as idx_f, p_bin.open("rb") as bin_f:
         for idx_line in idx_f:
             fields = idx_line.rstrip().split(";")
             _hash = fields[3]
-            if len(fields) > 4:
-                _hash = fields[4]
+            filenames = tuple(PurePosixPath(x) for x in fields[4:])
             offset = int(fields[1])
             length = int(fields[2])
+            if not blobfilter(_hash):
+                continue
+            if not filefilter(filenames):
+                continue
             bin_f.seek(offset, os.SEEK_SET)
             val = oscar.decomp(bin_f.read(length))
-            yield (_hash, val)
+            yield blobresult(_hash, val, filenames)
 
-def all_blobs():
+def all_blobs(**kwargs):
     for sec in all_blob_sections:
-        for i in iter_blobs(sec):
+        for i in iter_blobs(sec, **kwargs):
             yield i
 
 if __name__ == "__main__":
