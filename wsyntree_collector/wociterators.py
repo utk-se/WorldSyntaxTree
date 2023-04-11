93c7e563ec35c5e3b3e75651890626d7faa5802a
Ben Klein <robobenklein@gmail.com>
2023-04-10 23:49:10 -0400

    binary_line_iterator

--- wsyntree_collector/wociterators.py
+++ wsyntree_collector/wociterators.py
@@ -13,6 +13,27 @@ blob_fbase = "/da5_data/All.blobs/blob_{section}.{ext}"
 
 blobresult = namedtuple("BlobResult", ["hash", "content", "filenames"])
 
+def binary_line_iterator(open_file):
+    """only yields lines that are decodable"""
+    chunksize = 4096
+    lineno = 0
+    buffer = bytearray()
+    for block in iter(lambda: open_file.read(chunksize), b""):
+        buffer += block
+        while b"\n" in buffer:
+            line, buffer = buffer.split(b"\n", 1)
+            lineno += 1
+            try:
+                yield line.decode()
+            except UnicodeDecodeError:
+                log.warn(f"cannot decode line {lineno} of {open_file.name}")
+    if buffer:
+        for line in buffer.split(b"\n"):
+            try:
+                yield line.decode()
+            except UnicodeDecodeError:
+                log.warn(f"cannot decode line {lineno} of {open_file.name}")
+
 def iter_blobs(section, blobfilter=lambda b: True, filefilter=lambda fnames: True):
     """
     blobfilter(str) -> bool
@@ -23,8 +44,9 @@ def iter_blobs(section, blobfilter=lambda b: True, filefilter=lambda fnames: Tru
     p_idx = Path(blob_fbase.format(section=section, ext="idxf"))
     p_bin = Path(blob_fbase.format(section=section, ext="bin"))
 
-    with gzip.open(p_idx, "rt") as idx_f, p_bin.open("rb") as bin_f:
-        for idx_line in idx_f:
+    # open the index in binary as well, encoding not specified
+    with gzip.open(p_idx, "rb") as idx_f, p_bin.open("rb") as bin_f:
+        for idx_line in binary_line_iterator(idx_f):
             fields = idx_line.rstrip().split(";")
             _hash = fields[3]
             filenames = tuple(PurePosixPath(x) for x in fields[4:])
